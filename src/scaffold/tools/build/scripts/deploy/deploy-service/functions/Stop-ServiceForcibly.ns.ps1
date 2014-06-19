e20c0f53364c03bf3d67da5710d63b9b5f55d0c3
mwpeng <mwpeng@thoughtworks.com>
2014-06-19 14:06:38 +0800

    [xqyu] fix

--- src/scaffold/tools/build/scripts/deploy/deploy-service/functions/Stop-ServiceForcibly.ns.ps1
+++ src/scaffold/tools/build/scripts/deploy/deploy-service/functions/Stop-ServiceForcibly.ns.ps1
@@ -7,16 +7,23 @@ Function Stop-ServiceForcibly($name){
         }
         try {
             $service.WaitForStatus("Stopped", $waitingTimeSpan)
+            $wmiObj = Get-WmiObject Win32_Service -Filter "Name='$name'"
+            if ($wmiObj.ProcessId -ne 0) {
+                throw "Service is stopped, but process still running. "
+            }
         } catch [System.ServiceProcess.TimeoutException]{
             Write-Host "Cannot stop service $name. Start to kill it." 
             $wmiObj = Get-WmiObject Win32_Service -Filter "Name='$name'"
             if ($wmiObj.ProcessId -ne 0) {
-                Stop-Process -Id $wmiObj.ProcessId -Force
-                $exited = (Get-Process -Id $wmiObj.ProcessId).WaitForExit(10000)
-                if (-not $exited) {
-                    throw "Process cannot be killed. Stop service failed. "
+                $process = Get-Process -Id $wmiObj.ProcessId -ErrorAction SilentlyContinue
+                if ($process) {
+                    Stop-Process -Id $process.ProcessId -Force -ErrorAction SilentlyContinue
+                    $exited = $process.WaitForExit(10000)
+                    if (-not $exited) {
+                        throw "Process cannot be killed. Stop service failed. "
+                    }
                 }
-            }                
+            }
         }   
         catch {
             throw $_
